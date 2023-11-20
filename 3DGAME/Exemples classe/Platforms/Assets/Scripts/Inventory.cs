788828add92bacde069eb86b8232184e9715e25a
pol.forner <pol.forner@estudiantat.upc.edu>
2023-11-20 17:58:19 +0100

    Commit exemples profe + modelos 3D

---
+++ 3DGAME/Exemples classe/Platforms/Assets/Scripts/Inventory.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    public int ammunition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ammunition = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
